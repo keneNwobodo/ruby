bc5a5e1f922fa856def37a24f6b63f07ce8dba0b
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-08-03 17:54:16 +0100

    worked on caesat cipher mini project

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,4 +1,49 @@
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# MINI PROJECT
+
+# conver the strings into indiviual characters
+# map through the strings 
+# convert each string into numbers
+# rotate eac of those numbers
+# shift the numbes to x positions
+# convert back to strings 
+# join the strings
+
+#convert strings to numbers
+def str_to_numbers(str)
+   nums = str.collect { |letter| letter.ord}
+   return nums
+end
+
+# move the numbers to target positions
+def mov_positions(nums, target)
+   shifted_nums = nums.collect { |num| num + target}
+   return shifted_nums
+end
+
+# shifted strings before conversion
+def shift_nums(nums)
+   shifted = nums.collect {|num| num.chr}
+   return shifted.join
+end
+
+# caesar function 
+def caesar_cipher(str, target)
+    str = str.downcase.split("")
+
+# invoke func of strings to numbers   
+nums = str_to_numbers(str)
+
+# invoke the shift positions
+shift_position = mov_positions(nums, target)
+
+# invoke the shift func
+shifted = shift_nums(shift_position)
+
+  return shifted
+end
+
+puts caesar_cipher("What a string!", 5)
+
 
 
 
